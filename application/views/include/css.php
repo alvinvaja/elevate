c327aa3fc2573c600ed5084345690f1bc0296070
alvinvaja <alvinvaja@gmail.com>
2019-10-30 13:26:08 +0700

    Implement homepage header, add logo assets

--- application/views/include/css.php
+++ application/views/include/css.php
@@ -1,3 +1,7 @@
+<!-- Bootstrap -->
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/bootstrap-grid.min.css') ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/bootstrap-reboot.min.css') ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/bootstrap.min.css') ?>">
+
+<!-- Local CSS -->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/style.css') ?>">
--- application/views/pages/header.php
+++ application/views/pages/header.php
@@ -1,3 +1,24 @@
-<?php
-    echo "Ini header ya mas<br>";
-?>
+<div class="header black">
+    <p>Free Shipping</p>
+    <p>Login</p>
+</div>
+<div class="header__nav">
+    <div class="logo__elevate">
+        <img src="
+            <?php echo base_url('/assets/logo/elevate.png'); ?>
+        ">
+    </div>
+
+    <div class="navbar">
+        <div>Home</div>
+        <div>Shop</div>
+        <div>Help</div>
+        <div>About</div> 
+    </div>
+
+    <div class="logo__cart">
+        <img src="
+            <?php echo base_url('/assets/logo/cart.png'); ?>
+        ">
+    </div>
+</div>
