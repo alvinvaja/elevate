764138bd87116d9a20661acdd66b2b228cbcf71c
alvinvaja <alvinvaja@gmail.com>
2019-10-30 12:09:11 +0700

    Add bootstrap css and js support

---
+++ application/views/include/css.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/bootstrap-grid.min.css') ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/bootstrap-reboot.min.css') ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/bootstrap.min.css') ?>">
