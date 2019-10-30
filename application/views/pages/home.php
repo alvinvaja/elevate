fe5ced0f40a4541bcbb1f49aac7031f13daf3c4f
alvinvaja <alvinvaja@gmail.com>
2019-10-30 12:20:37 +0700

    Add homepage template

---
+++ application/views/pages/home.php
@@ -0,0 +1,17 @@
+<!D0CTYPE html>
+<html>
+<head>
+    <?php
+        echo $js;
+        echo $css;
+    ?>
+    <title>ELEVATE Indonesia: Belanja Fashion Online Terlengkap di Indonesia</title>
+</head>
+<body>
+    <?php
+        echo $header;
+        echo $content;
+        echo $footer;
+    ?>
+</body>
+</html>
