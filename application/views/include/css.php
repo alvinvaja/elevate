439ede6413ed2f68e1e928a18d77b019fb2e6c53
alvinvaja <alvinvaja@gmail.com>
2019-11-03 02:13:44 +0700

    Add responsive footer (desktop and tablet only)   *TODO : mobile version

--- application/views/include/css.php
+++ application/views/include/css.php
@@ -5,3 +5,4 @@
 
 <!-- Local CSS -->
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/style.css') ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/style__footer.css') ?>">
--- application/views/pages/footer.php
+++ application/views/pages/footer.php
@@ -1,3 +1,43 @@
-<?php
-    echo "Ini footer ya mas<br>";
-?>
+<div class="footer">
+    <div>
+        <h1>CATEGORIES</h1>
+        <p>Women</p>
+        <p>Men</p>
+        <p>Accessories</p>
+        <p>Footwear</p>
+    </div>
+    <div>
+        <h1>HELP</h1>
+        <p>Track Order</p>
+        <p>Returns</p>
+        <p>Shipping</p>
+        <p>FAQ</p>
+    </div>
+    <div>
+        <h1>GET IN TOUCH</h1>
+        <p>
+            Any questions? Let us know in store at 8th floor, 
+            379 Hudson St, New York, NY 10018 or call us on
+            (+1) 96 716 6879
+        </p>
+        <div class="social__media">
+            <img src="
+                <?php echo base_url('/assets/logo/facebook.png'); ?>
+            ">
+            <img src="
+                <?php echo base_url('/assets/logo/insta.png'); ?>
+            ">
+            <img src="
+                <?php echo base_url('/assets/logo/pinte.png'); ?>
+            ">
+        </div>
+    </div>
+    <div>
+        <h1>NEWSLETTER</h1>
+        <form action="subscribe.php" method="post">
+            <input type="text" name="email" placeholder="example@gmail.com" />
+            <br/>
+            <button type="submit" value="SUBSCRIBE" class="subsButton">SUBSCRIBE</button>
+        </form>
+    </div>
+</div>
