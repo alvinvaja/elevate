e11af40b8a7ef343366a20465aa19d212cf3c33e
unknown <alvinvaja@gmail.com>
2019-11-17 18:38:49 +0700

    Add login page

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,12 +11,37 @@ class Home extends CI_Controller {
         $data['footer'] = $this->load->view('pages/footer.php', NULL, TRUE);
         $this->load->view('pages/home.php', $data);
     }
-    public function shopCart()
-    {
+
+    public function shopCart() {
         $data['js'] = $this->load->view('include/javascript.php', NULL, TRUE);
         $data['css'] = $this->load->view('include/css.php', NULL, TRUE);
         $data['header'] = $this->load->view('pages/header.php', NULL, TRUE);
         $data['footer'] = $this->load->view('pages/footer.php', NULL, TRUE);
         $this->load->view('pages/shopcart.php', $data);
+    }
+
+    public function subscribe() {
+        $config = Array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_port' => 465,
+            'smtp_user' => 'xxx',
+            'smtp_pass' => 'xxx',
+            'mailtype'  => 'html', 
+            'charset'   => 'iso-8859-1'
+        );
+        $this->load->library('email', $config);
+        $this->email->set_newline("\r\n");
+
+        $email = $this->input->post('email');
+
+        $this->email->from('elevatee.id@gmail.com', 'Elevate Indonesia');
+        $this->email->to($email);
+
+        $this->email->subject('Test');
+        $this->email->message('Mawar for the win!');
+
+        $this->email->send();
+        redirect('http://localhost/elevate');
     }
 }
